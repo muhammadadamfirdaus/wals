357121fc6b056dd899ccef3fd772d41ab522d91f
muhammadadamfirdaus <adam@muhammadadamfirdaus.com>
2021-09-27 08:49:47 +0700

    product page

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,9 +1,33 @@
+import Image from "next/image";
+import { Container, Row } from "react-bootstrap";
+import styles from "../../styles/scss/FeaturedImage.module.scss";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
 import Layout from "../../components/Layout";
 
 export default function Products() {
 	return (
-		<Layout>
-			<h1>Our Products</h1>
+		<Layout title="Jasa PT WALS.">
+			<Header />
+			<div className="content">
+				<div className="wrapper">
+					<figure className={styles.featuredImage}>
+						<h1 className={styles.title}>Jasa PT WALS</h1>
+						<Image
+							src="/images/headline.jpg"
+							layout="fill"
+							objectFit="cover"
+							alt="Jasa PT WALS."
+						/>
+					</figure>
+					<Container>
+						<Row>
+							<h1 className="title">Jasa PT WALS</h1>
+						</Row>
+					</Container>
+				</div>
+			</div>
+			<Footer />
 		</Layout>
 	);
 }
